bc4ee543ce5beec823ae0cbd811bb7febe43103f
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2014-07-04 15:42:47 +0200

    get-version.sh will always work in the directory it's placed in

--- get-version.sh
+++ get-version.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Always create version string for repository this script lies in,
+# not the cwd... Makes usage easier in cmake
+ARG0="$0"
+SELF="$(readlink -f "${ARG0}")"
+ROOT_DIR="$(dirname "${SELF}")"
+cd "$ROOT_DIR"
+
 [ -n "$(git diff)" ] && MODDED='+MOD'
 
 echo $(git describe)$MODDED, branch $(git rev-parse --abbrev-ref HEAD), built "$(date +%Y-%m-%d)"
