d6d983677e0412fdb08b4bcf02bcbaae4e36b3c4
schwaer@HiWi.de <schwaer@HiWi.de>
2015-04-24 15:07:21 +0200

    Fuse

---
+++ src/fuse/imageHelper.h
@@ -0,0 +1,28 @@
+#ifndef IMAGEHELPER_H
+#define IMAGEHELPER_H
+
+#include <netdb.h>
+#include "protocol.h"
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+
+typedef struct log_info {
+  uint64_t imageSize;
+  uint64_t receivedBytes;
+  uint64_t imageBlockCount;
+  uint8_t * blockRequestCount;
+} log_info;
+
+
+
+void printLog(log_info *info);
+
+bool sock_printable(struct sockaddr *addr, socklen_t addrLen, char *output, int len);
+
+int connect_to_server(char ** server_adress, int * port);
+#endif
