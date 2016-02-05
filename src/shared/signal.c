68a3c389103843233d03b07bf4eda7d9883a161a
Simon Rettberg <rettberg@informatik.uni-freiburg.de>
2016-02-05 21:41:16 +0100

    [SHARED] Fix linux detection on clang

--- src/shared/signal.c
+++ src/shared/signal.c
@@ -1,13 +1,13 @@
 #include "signal.h"
 
-#if defined(linux)
-//#warning "Using eventfd based signalling"
+#if defined(linux) || defined(__linux) || defined(__linux__)
+#warning "Using eventfd based signalling"
 #include "signal.inc/eventfd.c"
 #elif __SIZEOF_INT__ == 4 && __SIZEOF_POINTER__ == 8
-//#warning "Using pointer-packing pipe based signalling"
+#warning "Using pointer-packing pipe based signalling"
 #include "signal.inc/pipe64.c"
 #else
-//#warning "Using fallback pipe based signalling"
+#warning "Using fallback pipe based signalling"
 #include "signal.inc/pipe_malloc.c"
 #endif
 
